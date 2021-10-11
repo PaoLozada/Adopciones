b9b4978d48ab38c1ff64cc830c582597e8ecef1b
paolozada <palozadagutierrez@gmail.com>
2021-10-11 07:10:01 -0500

    make it better

--- appAdopciones/models/mascotas.py
+++ appAdopciones/models/mascotas.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Mascotas(models.Model):
-    Id_Mascota = models.CharField(primary_key=True,max_length=20)
+    Id_Mascota = models.AutoField(primary_key=True, default=0)
     Nombre = models.CharField('Nombre', max_length=50)
     Edad = models.IntegerField(default=0)
     Disponibilidad = models.BooleanField(default=True)
