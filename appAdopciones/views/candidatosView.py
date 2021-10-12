d68af7e4b528229320440b475cf2eeb7a35c337c
paolozada <palozadagutierrez@gmail.com>
2021-10-11 20:00:22 -0500

    make it better

--- appAdopciones/views/candidatosView.py
+++ appAdopciones/views/candidatosView.py
@@ -28,7 +28,11 @@ class CandidatosView (views.APIView):
     def get(self, request, *args, **kwargs):
         serializer = CandidatosSerializer(Candidatos.objects.all(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+   
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 
     
