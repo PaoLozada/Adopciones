05623ddc3f0527e544718c248d641879b753da15
paolozada <palozadagutierrez@gmail.com>
2021-10-11 20:59:37 -0500

    make it better

--- appAdopciones/views/mascotaView.py
+++ appAdopciones/views/mascotaView.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from appAdopciones.serializers.mascotaSerializer import MascotaSerializer
+from appAdopciones.models.mascotas import Mascotas
 
 class MascotaView (views.APIView):
     def post (self, request, *args, **kwargs):
@@ -11,8 +12,13 @@ class MascotaView (views.APIView):
         serializer.is_valid(raise_exception= True)
         serializer.save()
 
-        tokenData = {"username": request.data["userData"], "pasword": request.data["pasword"]}
+        '''tokenData = {"username": request.data["userData"], "pasword": request.data["pasword"]}
         tokenSerializer = TokenObtainPairSerializer(data = tokenData)
         tokenSerializer.is_valid(raise_exception=True)
 
-        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)'''
+        return Response(status=status.HTTP_201_CREATED)
+
+    def get(self, request, *args, **kwargs):
+        serializer = MascotaSerializer(Mascotas.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
