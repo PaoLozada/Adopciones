ee19d7831f8bfa42de902be2c28244c60cad815d
paolozada <palozadagutierrez@gmail.com>
2021-10-12 22:20:54 -0500

    make it better

--- appAdopciones/serializers/candidatosSeerializer.py
+++ appAdopciones/serializers/candidatosSeerializer.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class CandidatosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidatos
-        fields = ['Numero_Identificacion', 'Nombre_Completo', 'Direccion','Numero_Contacto','Email','Edad','Resultado_Prueba']  
+        fields = ['Id_Candidato','Numero_Identificacion', 'Nombre_Completo', 'Direccion','Numero_Contacto','Email','Edad','Resultado_Prueba']  
       
