ef91334efb0ce61a0f4fddf1eb00edfaf22cb4c6
Plozada <Plozada>
2021-10-18 00:36:15 -0500

    make it better

--- appAdopciones/serializers/userSerializer.py
+++ appAdopciones/serializers/userSerializer.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     solicitud = SolicitudSerializer
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'name', 'email','solicitud']
+        fields = ['username', 'password', 'name', 'email']
 
     def create(self, validated_data):
         solicitudData = validated_data.pop ('solicitud')
@@ -20,19 +20,11 @@ class UserSerializer(serializers.ModelSerializer):
         solicitud = Solicitud.objects.get(user= obj.id)
         
         return{
-            'id': user.id,
             'username' : user.username,
             'password' : user.password,
             'name' : user.name,
-            'email' : user.email,
-            'solicitud':{
-                'id_Solicitud' : solicitud.id_Solicitud,
-                'id_User' : solicitud.id_User,
-                'id_Mascotas' : solicitud.id_Mascotas,
-                'id_Candidatos' : solicitud.id_Candidatos,
-                'estado' : solicitud.estado,
-                'respuesta' : solicitud.respuesta
-            }
+            'email' : user.email
+            
         }
 
 
