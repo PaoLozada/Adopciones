2a5d4964d47611eecd056a831cf65a29439cbd7e
Plozada <Plozada>
2021-10-18 00:44:13 -0500

    make it better

--- appAdopciones/serializers/userSerializer.py
+++ appAdopciones/serializers/userSerializer.py
@@ -1,15 +1,14 @@
 from appAdopciones.models.user import User
 from rest_framework import serializers
 from appAdopciones.models.solicitud import Solicitud
-from appAdopciones.serializers.solicitudSerializer import SolicitudSerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
-    solicitud = SolicitudSerializer
     class Meta:
         model = User
         fields = ['username', 'password', 'name', 'email']
 
-    def create(self, validated_data):
+    '''def create(self, validated_data):
         solicitudData = validated_data.pop ('solicitud')
         userInstance = User.objects.create(**validated_data)
         Solicitud.objects.create(user= userInstance, **solicitudData)
@@ -17,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def to_representation(self, obj):
         user = User.objects.get(id=obj.id)
-        solicitud = Solicitud.objects.get(user= obj.id)
+        
         
         return{
             'username' : user.username,
@@ -25,8 +24,4 @@ class UserSerializer(serializers.ModelSerializer):
             'name' : user.name,
             'email' : user.email
             
-        }
-
-
-        
-       
+        }'''
