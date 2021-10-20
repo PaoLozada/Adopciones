78acec877e1309051c22ccd47a59e2f81d183b82
Plozada <plozada>
2021-10-19 22:32:55 -0500

    make it better

--- appAdopciones/views/candidatosView.py
+++ appAdopciones/views/candidatosView.py
@@ -7,22 +7,34 @@ from appAdopciones.models.candidatos import Candidatos
 from appAdopciones.serializers.candidatosSeerializer import CandidatosSerializer
 
 class CandidatosView (views.APIView):
-    def post (self, request, *args, **kwargs):
+
+    
+    def get(self, request, format=None):
+        candidato = Candidatos.objects.all()
+        serializer = CandidatosSerializer(candidato, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = CandidatosSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    '''def post (self, request, *args, **kwargs):
         serializer = CandidatosSerializer (data = request.data)
         serializer.is_valid(raise_exception= True)
         serializer.save()
 
-        '''tokenData = {"username": request.data["userData"], "pasword": request.data["pasword"]}
-        tokenSerializer = TokenObtainPairSerializer(data = tokenData)
-        tokenSerializer.is_valid(raise_exception=True)
-
-        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)'''
-        return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_201_CREATED)'''
 
     '''def get(self, request, Id_Candidato, format=None):
         candidato = self.get_object(Id_Candidato)
         serializer = CandidatosSerializer(candidato)
         return Response(serializer.data)'''
+
+
     def get_object(self, Id_Candidato):
         try:
             return Candidatos.objects.get(Id_Candidatos=Id_Candidato)
@@ -30,10 +42,7 @@ class CandidatosView (views.APIView):
             raise ("Http404")
 
 
-    def get(self, request, *args, **kwargs):
-        serializer = CandidatosSerializer(Candidatos.objects.all(), many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
     def put(self, request, pk, format=None):
         candidato = self.get_object(pk)
         serializer = CandidatosSerializer(candidato, data=request.data)
