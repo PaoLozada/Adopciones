994e39993c1560640375904845f23fabe5eeb71d
Plozada <plozada>
2021-10-21 08:06:43 -0500

    make it better

--- appAdopciones/serializers/candidatosSeerializer.py
+++ appAdopciones/serializers/candidatosSeerializer.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class CandidatosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidatos
-        fields = ['Numero_Identificacion', 'Nombre_Completo', 'Direccion', 'Numero_Contacto', 'Email', 'Edad', 'Has_Tenido_Mascotas', 'Seras_Responsable', 'Tienes_Espacio', 'Tienes_Tiempo', 'Recursos_Economicos', 'Afrontar_Problemas', 'Resultado_Prueba']  
+        fields = ['Numero_Identificacion', 'Nombre_Completo', 'Direccion', 'Numero_Contacto', 'Email', 'Edad', 'Has_Tenido_Mascotas', 'Seras_Responsable', 'Tienes_Espacio', 'Tienes_Tiempo', 'Recursos_Economicos', 'Afrontar_Problemas', 'Resultado_Prueba','Resultado_Prueba']  
       
     '''def to_representation(self, obj):
         candi = Candidatos.objects.get(Id_Candidato = obj.Id_Candidato)
