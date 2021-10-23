992f77aee56e2f2ee0ef7b7efb6fb33e9790dbfd
Plozada <plozada>
2021-10-23 17:29:03 -0500

    make it better

--- appAdopciones/models/user.py
+++ appAdopciones/models/user.py
@@ -28,7 +28,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.BigAutoField(primary_key=True)
-    username = models.CharField('Username', max_length = 15, unique=True)
+    username = models.CharField('Username', max_length = 15)
     password = models.CharField('Password', max_length = 256)
     name = models.CharField('Name', max_length = 30)
     email = models.EmailField('Email', max_length = 100)
