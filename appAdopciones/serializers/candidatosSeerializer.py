eb72510d976c011bbccf7ba1660ec43ec9fc1260
Plozada <plozada>
2021-10-24 20:27:12 -0500

    make it better

--- appAdopciones/serializers/candidatosSeerializer.py
+++ appAdopciones/serializers/candidatosSeerializer.py
@@ -6,11 +6,7 @@ class CandidatosSerializer(serializers.ModelSerializer):
         model = Candidatos
         fields = ['Numero_Identificacion', 'Nombre_Completo', 'Direccion', 'Numero_Contacto', 'Email', 'Edad', 'Has_Tenido_Mascotas', 'Seras_Responsable', 'Tienes_Espacio', 'Tienes_Tiempo', 'Recursos_Economicos', 'Afrontar_Problemas', 'Resultado_Prueba']  
       
-    '''def to_representation(self, obj):
-        candi = Candidatos.objects.get(Id_Candidato = obj.Id_Candidato)
-        return{
-            'Nombrecito': candi.Nombre_Completo,
-            'Edadcita': candi.Numero_Identificacion,
-            'Disponibilidadcita': candi.Email
+def calcular():
+    Candidatos.Resultado_Prueba= Candidatos.Has_Tenido_Mascotas + Candidatos.Seras_Responsable + Candidatos.Tienes_Espacio 
 
-        }'''
+calcular()
